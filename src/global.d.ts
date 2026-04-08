76d20ebfe030c9c21fe47b4d6a4aa2e87e55d2da
salamentic <raniruddh99@gmail.com>
2026-04-08 17:37:26 -0400

    Added type descriptions

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,3 +3,15 @@ declare module '*.wav' {
   const src: string;
   export default src;
 }
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
